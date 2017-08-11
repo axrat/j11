e24681efbe51e36e90b72059b7e3e28f2572577a
onoie <onoie3@gmail.com>
2017-08-12 02:17:49 +0900

    add python

---
+++ bash/alias.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+alias ls='ls --color=auto'
+alias ll='ls -alFh'
+alias la='ls -A'
+alias l='ls -cf'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias docker='sudo docker'
+
