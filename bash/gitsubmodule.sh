d7bf3e7520d10d5306de9cc6d964152ee526e7b7
onoie <onoie3@gmail.com>
2017-09-30 09:55:21 +0900

    fast commit

--- bash/gitsubmodule.sh
+++ bash/gitsubmodule.sh
@@ -1,3 +1,11 @@
+
+gitsubmoduleadd(){
+  if [ $# -ne 3 ]; then
+    echo "Require [RepoHost],[RepoUser],[RepoName]"
+  else
+	git submodule add https://$1/$2/$3.git $3
+  fi
+}
 gitsubmoduleinit(){
   git submodule update --init --recursive
 }
