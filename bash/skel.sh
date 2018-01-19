dd654b7474e0e3148ae826551349892cb3859ed3
onoie <onoie3@gmail.com>
2018-01-19 22:33:27 +0900

    fix goskel

--- bash/skel.sh
+++ bash/skel.sh
@@ -80,13 +80,19 @@ EOF
 skelgo(){
 cat >> main.go << 'EOF'
 package main
-
+import (
+	"github.com/onoie/goz"
+)
+import (
+	_ "github.com/mattn/go-sqlite3"
+)
 import (
 	"fmt"
 )
 
 func main() {
 	fmt.Println("HelloWorld")
+	goz.Complete()
 }
 EOF
 }
