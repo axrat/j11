ef7355adc8999b5ebff239aef68cd3918440e415
onoie <onoie3@gmail.com>
2018-03-05 02:52:04 +0900

    restore

--- setup.sh
+++ setup.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
+
 sudo chmod 777 $(cd $(dirname $0); pwd) -Rf
-sudo find /v/ -type d -exec sudo chmod 777 {} +
-sudo find /v/ -type f -exec sudo chmod 666 {} +
+sudo find /j11/ -type d -exec sudo chmod 777 {} +
+sudo find /j11/ -type f -exec sudo chmod 666 {} +
 sudo chmod 700 ${0##*/}
+
+
+sudo chmod +x /j11/dotfiles/install.sh
+sudo chmod +x /j11/dotfiles/uninstall.sh
+
+#nanorc
+if [ -d ~/.nano ]; then
+  echo found ~/.nano
+else
+  DOWNLOAD=nanorc.zip
+  if [ -e $DOWNLOAD ]; then
+    echo "found $DOWNLOAD"
+  else
+    echo "not found $DOWNLOAD"
+    wget --no-check-certificate https://github.com/nanorc/nanorc/archive/master.zip -O nanorc.zip
+    unzip $DONWLOAD
+    cd nanorc-master
+    make install
+    cd ..
+    rm -rf nanorc-master
+    rm -f $DOWNLOAD
+  fi
+fi
