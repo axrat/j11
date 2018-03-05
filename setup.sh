bf38b79ce76e773e3d066150dab1f45758262a06
onoie <onoie3@gmail.com>
2018-03-06 08:50:30 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo chmod 777 $(cd $(dirname $0); pwd) -Rf
-sudo find /j11/ -type d -exec sudo chmod 777 {} +
-sudo find /j11/ -type f -exec sudo chmod 666 {} +
+#sudo find /j11/ -type d -exec sudo chmod 777 {} +
+#sudo find /j11/ -type f -exec sudo chmod 666 {} +
 sudo chmod 700 ${0##*/}
 
 
