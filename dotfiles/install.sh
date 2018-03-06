487500eb95aefd2605e9c1250b7a33ba781f0e4b
onoie <onoie3@gmail.com>
2018-03-06 09:31:12 +0900

    add dein install script

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -10,3 +10,14 @@ ln -s /j11/dotfiles/dein_lazy.toml ~/.dein_lazy.toml
 ln -s /j11/dotfiles/emacs ~/.emacs
 ln -s /j11/dotfiles/bash_completion ~/.bash_completion
 
+#dein
+DEIN_PATH=~/.vim/dein/repos/github.com/Shougo/dein.vim
+if [ -d $DEIN_PATH ]; then
+  echo "found $DEIN_PATH"
+else
+  DOWNLOAD=installer.sh
+  curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > $DOWNLOAD
+  chmod +x $DOWNLOAD
+  sh ./installer.sh ~/.vim/dein
+  rm $DOWNLOAD
+fi
--- dotfiles/uninstall.sh
+++ dotfiles/uninstall.sh
@@ -4,9 +4,9 @@ rm -f ~/.gitignore_global
 rm -f ~/.devilspie
 rm -f ~/.nanorc
 rm -f ~/.vimrc
+rm -Rf ~/.vim/dein
 rm -f ~/.vim
 rm -f ~/.dein.toml
 rm -f ~/.dein_lazy.toml
 rm -f ~/.emacs
 rm -f ~/.bash_completion
-
