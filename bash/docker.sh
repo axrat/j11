8da4b332f84470d2cd6c7338af81f4f54227990b
onoie <onoie3@gmail.com>
2018-03-24 22:22:52 +0900

    mv

--- bash/docker.sh
+++
@@ -1,4 +0,0 @@
-
-dockernonermiall(){
-  docker images | awk '/<none/{print $3}' | xargs sudo docker rmi
-}
--- bash/var.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export LC_MESSAGES="en_US.UTF-8"
-LINES=$(tput lines)
-COLUMNS=$(tput cols)
-PRESS_KEY=" echo 'Press any key to continue...'; read -k1 -s"
-
